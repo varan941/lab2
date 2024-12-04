e7a6560eb58108c08aacc6262b12eb9dc123b052
Ivan Teslenko <varan941@gmail.com>
2024-12-04 22:19:31 +0200

    Added Program class

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CodeWars0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string text = "8 3 -5 42 -1 0 0 -9 4 7 4 -4";
+
+            
+        }
+
+        string HighAndLow(string numbers)
+        {
+            // Code here or    
+            var nums = numbers.Split(new char[] { ' ' });
+
+            int min=Convert.ToInt32(nums[0]), max=Convert.ToInt32(nums[0]);            
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num;
+                Int32.TryParse(nums[i], out num);
+
+                if (max < num)
+                {
+                    max = num;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num;
+                Int32.TryParse(nums[i], out num);
+
+                if (min > num)
+                {
+                    min = num;
+                }
+            }
+
+            Console.WriteLine(max);
+
+            return "throw towel";
+        }
+    }
+}
